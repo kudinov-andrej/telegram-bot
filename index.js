9740604b75d4e8b8a74b02c7a67d0de04b0a14b0
Andrej Kudinov <kudinov.andrej@list.ru>
2023-05-06 15:37:40 +0300

    добавил кнопки

--- index.js
+++ index.js
@@ -6,21 +6,74 @@ const token = '5985534409:AAFH9VPDBOa0QDmbU34hnaV5tCnLK5cvg8Q';
 
 const bot = new TelegramApi(token, { polling: true })
 
-// определяем команды бота
-bot.setMyCommands([
-    { command: '/start', description: 'Начальное приветствие' },
-    { command: '/whatmedo', description: 'Когда не знаешь, что делать' },
-    { command: '/quote', description: 'Когда нужна светлая мысль' },
-])
-// ставим слушатель на сообщения в боте
+const button = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{ text: 'Старт', callback_data: '/start' },
+            { text: 'Цитата', callback_data: '/quote' },
+            { text: 'Что делать?', callback_data: '/whatmedo' }]
+        ]
+    })
+}
 
-bot.on('message', async msg => {
-    const text = msg.text;
-    const chatId = msg.chat.id;
-    const lastName = msg.from.last_name;
-    const fistName = msg.from.first_name;
-    // используем метод отправки стикера sendSticker
-    if (text === '/start') {
+const start = async () => {
+
+    // определяем команды бота
+    bot.setMyCommands([
+        { command: '/start', description: 'Начальное приветствие' },
+        { command: '/whatmedo', description: 'Когда не знаешь, что делать' },
+        { command: '/quote', description: 'Когда нужна светлая мысль' },
+    ])
+    // ставим слушатель на сообщения в боте
+
+    bot.on('message', async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+        const lastName = msg.from.last_name;
+        const fistName = msg.from.first_name;
+        // используем метод отправки стикера sendSticker
+        if (text === '/start') {
+            await bot.sendSticker(chatId, 'https://cdn.tlgrm.app/stickers/2f8/602/2f8602cc-ede8-4bf0-b52f-0f4298fc15ec/thumb-animated-128.mp4');
+            if (lastName && fistName === undefined) {
+                // используем метод отправки сообщения sendMessage
+                await bot.sendMessage(chatId, `Привет, друг!`)
+            } else {
+                await bot.sendMessage(chatId, `Привет, ${lastName} ${fistName}!`)
+            }
+            return bot.sendMessage(chatId, `Добро пожаловать в лучший бот пространства Telegram. Приятного пользования!`, button)
+        }
+        if (text === '/whatmedo') {
+            const { data } = await axios.get('http://www.boredapi.com/api/activity?type=diy');
+            const joke = data.activity;
+            return bot.sendMessage(chatId, joke, button);
+
+        }
+        if (text === '/quote') {
+            const { data } = await axios.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text');
+            return bot.sendMessage(chatId, data, button);
+        }
+        return bot.sendMessage(chatId, 'Я тебя не понял, но ты можешь писать мне что хочется, если тебе от этого становится лучше. Я все читаю.', button);
+
+    });
+
+}
+
+bot.on('callback_query', async msg => {
+    const data = msg.data;
+    const chatId = msg.message.chat.id;
+    const lastName = msg.message.chat.last_name;
+    const fistName = msg.message.chat.first_name;
+    console.log(msg)
+    if (data === '/quote') {
+        const { data } = await axios.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text');
+        return bot.sendMessage(chatId, data, button);
+    }
+    if (data === '/whatmedo') {
+        const { data } = await axios.get('http://www.boredapi.com/api/activity?type=diy');
+        const joke = data.activity;
+        return bot.sendMessage(chatId, joke, button);
+    }
+    if (data === '/start') {
         await bot.sendSticker(chatId, 'https://cdn.tlgrm.app/stickers/2f8/602/2f8602cc-ede8-4bf0-b52f-0f4298fc15ec/thumb-animated-128.mp4');
         if (lastName && fistName === undefined) {
             // используем метод отправки сообщения sendMessage
@@ -28,17 +81,8 @@ bot.on('message', async msg => {
         } else {
             await bot.sendMessage(chatId, `Привет, ${lastName} ${fistName}!`)
         }
-        return bot.sendMessage(chatId, `Добро пожаловать в лучший бот пространства Telegram. Приятного пользования!`)
+        return bot.sendMessage(chatId, `Добро пожаловать в лучший бот пространства Telegram. Приятного пользования!`, button)
     }
-    if (text === '/whatmedo') {
-        const { data } = await axios.get('http://www.boredapi.com/api/activity?type=diy');
-        const joke = data.activity;
-        return bot.sendMessage(chatId, joke);
+})
 
-    }
-    if (text === '/quote') {
-        const { data } = await axios.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=text');
-        return bot.sendMessage(chatId, data);
-    }
-    return bot.sendMessage(chatId, 'Я тебя не понял, но ты можешь писать мне что хочется, если тебе от этого становится лучше. Я все читаю.');
-});
+start()
